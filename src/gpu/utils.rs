a13677fd0cef9541694328d5485acd8a38dfc1e1
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-09-29 15:27:17 +0330

    raise error when requested platform is not found

--- src/gpu/utils.rs
+++ src/gpu/utils.rs
@@ -5,11 +5,12 @@ pub const GPU_NVIDIA_PLATFORM_NAME : &str = "NVIDIA CUDA";
 pub const CPU_INTEL_PLATFORM_NAME : &str = "Intel(R) CPU Runtime for OpenCL(TM) Applications";
 
 pub fn get_devices(platform_name: &str) -> GPUResult<Vec<Device>> {
-    let nvidia = Platform::list().into_iter().find(|&p|
+    let platform = Platform::list().into_iter().find(|&p|
         match p.name() {
             Ok(p) => p == platform_name,
-            Err(e) => false
-        }).expect("No device found!");
-    let devices = Device::list_all(nvidia)?;
+            Err(_) => false
+        });
+    if platform.is_none() { return Err(GPUError {msg: "GPU platform not found!".to_string()}); }
+    let devices = Device::list_all(platform.unwrap())?;
     return Ok(devices);
 }
