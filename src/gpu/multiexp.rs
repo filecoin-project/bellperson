f2db3531b7a45fafcf67e50f22314f199f3b3303
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-10-18 16:26:44 +0330

    same multiexp kernel instance for all sizes

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -139,7 +139,7 @@ pub struct MultiexpKernel<E>(Vec<SingleMultiexpKernel<E>>) where E: Engine;
 
 impl<E> MultiexpKernel<E> where E: Engine {
 
-    pub fn create(n: u32) -> GPUResult<MultiexpKernel<E>> {
+    pub fn create() -> GPUResult<MultiexpKernel<E>> {
         let devices = utils::get_devices(utils::GPU_NVIDIA_PLATFORM_NAME)?;
         if devices.len() == 0 { return Err(GPUError {msg: "No working GPUs found!".to_string()} ); }
         let mut kernels = Vec::new();
