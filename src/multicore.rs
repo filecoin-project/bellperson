1a7679bd2b7e71c3d8bb39f227181cd38f9e868b
dignifiedquire <me@dignifiedquire.com>
2020-09-29 15:31:42 +0200

    remove crossbeam for gpu

--- src/multicore.rs
+++ src/multicore.rs
@@ -1,6 +1,6 @@
 //! An interface for dealing with the kinds of parallel computations involved in
 //! `bellperson`. It's currently just a thin wrapper around [`CpuPool`] and
-//! [`crossbeam`] but may be extended in the future to allow for various
+//! [`rayon`] but may be extended in the future to allow for various
 //! parallelism strategies.
 //!
 //! [`CpuPool`]: futures_cpupool::CpuPool
