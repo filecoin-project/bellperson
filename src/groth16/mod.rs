4bdfaa9bf3ff8398db6954da9f44d4ea84b7aecb
Natanael <neithanmo@gmail.com>
2021-09-30 15:29:08 -0600

    feat(scheduler):     - Include module     - Define a new enum use to tag the type of the task that is calling       the prover

--- src/groth16/mod.rs
+++ src/groth16/mod.rs
@@ -14,6 +14,7 @@ mod mapped_params;
 mod params;
 mod proof;
 mod prover;
+mod prover_ext;
 mod verifier;
 mod verifying_key;
 
@@ -25,5 +26,13 @@ pub use self::mapped_params::*;
 pub use self::params::*;
 pub use self::proof::*;
 pub use self::prover::*;
+pub(crate) use self::prover_ext::*;
 pub use self::verifier::*;
 pub use self::verifying_key::*;
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum BellTaskType {
+    WinningPost,
+    WindowPost,
+    MerkleTree,
+}
