26014029c960355e97074ccd13a8af562478748b
Natanael <neithanmo@gmail.com>
2021-09-30 15:37:19 -0600

    feat(scheduler): Update public API     - remove the in_priority suffix and boolean arguments intended to       express priority as the TaskType full-fill this purpose.       this task_type is also used by the scheduler to get the list of       devices users assign for this task as well as timeouts and       deadlines(the way the scheduler prioritize tasks)

--- src/groth16/ext.rs
+++ src/groth16/ext.rs
@@ -1,5 +1,6 @@
 use super::{create_proof_batch_priority, create_random_proof_batch_priority};
 use super::{ParameterSource, Proof};
+use crate::groth16::BellTaskType;
 use crate::{gpu, Circuit, SynthesisError};
 use pairing::MultiMillerLoop;
 use rand_core::RngCore;
@@ -15,7 +16,7 @@ where
     C: Circuit<E::Fr> + Send,
 {
     let proofs =
-        create_proof_batch_priority::<E, C, P>(vec![circuit], params, vec![r], vec![s], false)?;
+        create_proof_batch_priority::<E, C, P>(vec![circuit], params, vec![r], vec![s], None)?;
     Ok(proofs.into_iter().next().unwrap())
 }
 
@@ -30,7 +31,7 @@ where
     R: RngCore,
 {
     let proofs =
-        create_random_proof_batch_priority::<E, C, R, P>(vec![circuit], params, rng, false)?;
+        create_random_proof_batch_priority::<E, C, R, P>(vec![circuit], params, rng, None)?;
     Ok(proofs.into_iter().next().unwrap())
 }
 
@@ -44,7 +45,50 @@ where
     E: gpu::GpuEngine + MultiMillerLoop,
     C: Circuit<E::Fr> + Send,
 {
-    create_proof_batch_priority::<E, C, P>(circuits, params, r, s, false)
+    create_proof_batch_priority::<E, C, P>(circuits, params, r, s, None)
+}
+
+// Winning/window and tree_builders/hashers use this library. as a general use case, we should pass to
+// the scheduler the deadlines and task types. The deadline is the priority, the sooner the task must be completed
+// the more the scheduler will prioritize it.
+// the task_type is used to get the exclusive resources that a  task can use.
+// the rest of the API in this file remains the same, the no priority functions bellow, assign None as a
+// deadline, the other functions with the in_priority suffix set the deadline to now. both keep the
+// task_type as None, indicating there are not exclusive resources for them.
+
+// this function omits the deadline but takes in a task_type, for now the deadline is hardcoded in
+// the scheduler-client library according to the task_type. the scheduler will assigned the lowest
+// possible deadline to tasks that do not have a type. this function is called by rust-fil-proof
+// and it was easier to refactor the functions there to include only one parameter than modifying
+// them to take 2(deadline plus task_type)
+pub fn create_random_proof_batch_with_type<E, C, R, P: ParameterSource<E>>(
+    circuits: Vec<C>,
+    params: P,
+    rng: &mut R,
+    task_type: Option<BellTaskType>,
+) -> Result<Vec<Proof<E>>, SynthesisError>
+where
+    E: gpu::GpuEngine + MultiMillerLoop,
+    C: Circuit<E::Fr> + Send,
+    R: RngCore,
+{
+    create_random_proof_batch_priority::<E, C, R, P>(circuits, params, rng, task_type)
+}
+
+pub fn create_random_proof_with_type<E, C, R, P: ParameterSource<E>>(
+    circuit: C,
+    params: P,
+    rng: &mut R,
+    task_type: Option<BellTaskType>,
+) -> Result<Proof<E>, SynthesisError>
+where
+    E: gpu::GpuEngine + MultiMillerLoop,
+    C: Circuit<E::Fr> + Send,
+    R: RngCore,
+{
+    let proofs =
+        create_random_proof_batch_priority::<E, C, R, P>(vec![circuit], params, rng, task_type)?;
+    Ok(proofs.into_iter().next().unwrap())
 }
 
 pub fn create_random_proof_batch<E, C, R, P: ParameterSource<E>>(
@@ -57,61 +101,5 @@ where
     C: Circuit<E::Fr> + Send,
     R: RngCore,
 {
-    create_random_proof_batch_priority::<E, C, R, P>(circuits, params, rng, false)
-}
-
-pub fn create_proof_in_priority<E, C, P: ParameterSource<E>>(
-    circuit: C,
-    params: P,
-    r: E::Fr,
-    s: E::Fr,
-) -> Result<Proof<E>, SynthesisError>
-where
-    E: gpu::GpuEngine + MultiMillerLoop,
-    C: Circuit<E::Fr> + Send,
-{
-    let proofs =
-        create_proof_batch_priority::<E, C, P>(vec![circuit], params, vec![r], vec![s], true)?;
-    Ok(proofs.into_iter().next().unwrap())
-}
-
-pub fn create_random_proof_in_priority<E, C, R, P: ParameterSource<E>>(
-    circuit: C,
-    params: P,
-    rng: &mut R,
-) -> Result<Proof<E>, SynthesisError>
-where
-    E: gpu::GpuEngine + MultiMillerLoop,
-    C: Circuit<E::Fr> + Send,
-    R: RngCore,
-{
-    let proofs =
-        create_random_proof_batch_priority::<E, C, R, P>(vec![circuit], params, rng, true)?;
-    Ok(proofs.into_iter().next().unwrap())
-}
-
-pub fn create_proof_batch_in_priority<E, C, P: ParameterSource<E>>(
-    circuits: Vec<C>,
-    params: P,
-    r: Vec<E::Fr>,
-    s: Vec<E::Fr>,
-) -> Result<Vec<Proof<E>>, SynthesisError>
-where
-    E: gpu::GpuEngine + MultiMillerLoop,
-    C: Circuit<E::Fr> + Send,
-{
-    create_proof_batch_priority::<E, C, P>(circuits, params, r, s, true)
-}
-
-pub fn create_random_proof_batch_in_priority<E, C, R, P: ParameterSource<E>>(
-    circuits: Vec<C>,
-    params: P,
-    rng: &mut R,
-) -> Result<Vec<Proof<E>>, SynthesisError>
-where
-    E: gpu::GpuEngine + MultiMillerLoop,
-    C: Circuit<E::Fr> + Send,
-    R: RngCore,
-{
-    create_random_proof_batch_priority::<E, C, R, P>(circuits, params, rng, true)
+    create_random_proof_batch_priority::<E, C, R, P>(circuits, params, rng, None)
 }
