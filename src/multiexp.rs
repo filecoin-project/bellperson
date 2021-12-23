074c055b265ab1d1e883de8fdfa92fa4a77c6bf2
phoenixzhua <35123356+phoenixzhua@users.noreply.github.com>
2021-12-23 15:43:11 +0800

    fix the build error
    1. add an expected parameter for test label
    2. replace isWinPost with snake case name "is_win_post"

--- src/multiexp.rs
+++ src/multiexp.rs
@@ -446,11 +446,11 @@ fn test_with_bls12() {
     assert_eq!(naive, fast);
 }
 
-pub fn create_multiexp_kernel<E>(_log_d: usize, priority: bool, isWinPost: bool) -> Option<gpu::MultiexpKernel<E>>
+pub fn create_multiexp_kernel<E>(_log_d: usize, priority: bool, is_win_post: bool) -> Option<gpu::MultiexpKernel<E>>
 where
     E: Engine + gpu::GpuEngine,
 {
-    match gpu::MultiexpKernel::<E>::create(priority, isWinPost) {
+    match gpu::MultiexpKernel::<E>::create(priority, is_win_post) {
         Ok(k) => {
             info!("GPU Multiexp kernel instantiated!");
             Some(k)
